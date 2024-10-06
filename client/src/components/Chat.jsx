cb93fd060741fc864204813c63286f67ecb766de
Ritesh Pramanik <pramanikritesh907@gmail.com>
2024-10-06 22:22:43 +0530

    updated

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -13,7 +13,7 @@ export const Chat = ({photoURL,name,lastmsg, id}) => {
       </div>
       <div className="flex flex-col">
         <span className="text-[#7FFFAB] text-xl font-medium font-mont">{name}{" "}{user?.id === id ? "(You)" : ""}</span>
-        <span className="text-sm text-[#FFFFFF] font-mont">{lastmsg ? lastmsg : "Start your chat"}</span>
+        <span className="text-sm text-[#FFFFFF] font-mont">{lastmsg ? lastmsg?.length > 33 ? lastmsg.slice(0,33) : lastmsg  : "Start your chat"}</span>
       </div>
     </div>
   );
