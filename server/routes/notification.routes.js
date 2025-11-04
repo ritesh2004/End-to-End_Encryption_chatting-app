8bae079d096042b17d8d488f93b609e3d9ddb2a2
Ritesh Pramanik <pramanikritesh907@gmail.com>
2025-11-04 20:20:45 +0530

    router updated

--- server/routes/notification.routes.js
+++ server/routes/notification.routes.js
@@ -13,6 +13,15 @@ notificationRouter.post('/notification/upload-token', verify, async (req, res)=>
     }
 });
 
+notificationRouter.post('/notification/delete-token', verify, async (req, res)=>{
+    try {
+        const { data } = await axios.post(`${process.env.NOTIFICATION_SERVER_API_URL}/delete-token`, req.body)
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+});
+
 notificationRouter.post('/notification/send-notification', verify, async (req, res)=>{
     try {
         const { data } = await axios.post(`${process.env.NOTIFICATION_SERVER_API_URL}/send-notification`, req.body)
